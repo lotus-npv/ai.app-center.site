ed3597f898b0634e9b6e2dd7f9472becfd926a94
lotus-npv <lotus.npv@gmail.com>
2024-03-15 02:41:49 +0700

    new commit

--- src/helpers/fakebackend_helper.js
+++ src/helpers/fakebackend_helper.js
@@ -2,6 +2,10 @@ import axios from "axios";
 import { del, get, post, put } from "./api_helper";
 import * as url from "./url_helper";
 
+// demo api
+const getDemoData = () => get(url.GET_DEMO_DATA);
+export { getDemoData };
+
 // Gets the logged in user data from local session
 const getLoggedInUser = () => {
   const user = localStorage.getItem("user");
