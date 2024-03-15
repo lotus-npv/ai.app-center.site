29ffaea11c85acde56d5f82938d4b09c475acd7e
lotus-npv <lotus.npv@gmail.com>
2024-03-15 17:31:49 +0700

    new commit

--- src/pages/Ticket/CreateTicket.js
+++ src/pages/Ticket/CreateTicket.js
@@ -2,27 +2,29 @@ import React, { useEffect } from "react";
 import { Card, CardBody, Container, Button, CardHeader, Row, Col, UncontrolledTooltip } from "reactstrap";
 
 //redux
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector, useDispatch , shallowEqual} from "react-redux";
 
 // actions
-import { getDemoData } from "../../store/actions";
+import { getDemoData, getCareerAll } from "../../store/actions";
 
 const CreateTicket = (props) => {
 
     const dispatch = useDispatch();
-    const { demoData} = useSelector(state => ({
+    const { demoData, careerData} = useSelector(state => ({
         demoData: state.Demo.demoData,
-    }));
+        careerData: state.Career.datas
+    }), shallowEqual);
+
     // const { careerData } = useSelector(state => ({
     //     careerData: state.Career.datas
-    // }));
+    // }), shallowEqual);
 
     /*
     get data
     */
     useEffect(() => {
         dispatch(getDemoData());
-        // dispatch(getCareerAll());
+        dispatch(getCareerAll());
     }, [dispatch]);
 
     // your API's response data will be in events variable.
@@ -31,12 +33,12 @@ const CreateTicket = (props) => {
     return (
         <div className="page-content">
             <Container fluid={true}>
-                {/* <div>
+                <div>
                     <p>Career data</p>
                     {careerData.map(item => (
                         <p key={item.name}>{item.id} {' - '} {item.name}</p>
                     ))}
-                </div> */}
+                </div>
                 <div>
                     {demoData.map(item => (
                         <p key={item.id}>{item.id} {' - '} {item.title}</p>
