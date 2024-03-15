7890d8c841424fd623cb1e5924109b1512a3b312
lotus-npv <lotus.npv@gmail.com>
2024-03-15 18:11:49 +0700

    new commit

--- src/store/career/saga.js
+++ src/store/career/saga.js
@@ -2,11 +2,13 @@ import { takeEvery, put, call,all,fork  } from "redux-saga/effects";
 
 // Login Redux States
 import {
-    GET_CAREER_ALL,
+    GET_CAREER_ALL, SET_CAREER,
 } from "./actionTypes"
 import {
     getCareerAllFail,
     getCareerAllSuccess,
+    setCareerSuccess,
+    setCareerFail
 } from "./actions"
                                       
 import { getCareerDataAll, addNewCareer } from "../../helpers/fakebackend_helper";
@@ -19,13 +21,33 @@ function* fetCareerData() {
     yield put(getCareerAllFail(error))
   }
 }
-                                      
-export function* watchFetCareerData() {
-  yield takeEvery(GET_CAREER_ALL, fetCareerData);
+
+function* onAddNewCareer({ payload: data }) {
+  try {
+      const response = yield call(addNewCareer, data)
+      yield put(setCareerSuccess(response))
+      toast.success("Career Added Successfully", { autoClose: 2000 });
+  } catch (error) {
+      yield put(setCareerFail(error))
+      toast.error("Career Added Failed", { autoClose: 2000 });
+  }
 }
                                       
+// export function* watchFetCareerData() {
+//   yield takeEvery(GET_CAREER_ALL, fetCareerData);
+// }
+                                      
+// function* careerSaga() {
+//   yield all([fork(watchFetCareerData)]);
+// }
+
 function* careerSaga() {
-  yield all([fork(watchFetCareerData)]);
+  yield takeEvery(GET_CAREER_ALL, fetCareerData)
+  yield takeEvery(SET_CAREER, onAddNewCareer)
+  // yield takeEvery(UPDATE_JOB_LIST, onUpdateJobList)
+  // yield takeEvery(DELETE_JOB_LIST, onDeleteJobList)
+  // yield takeEvery(GET_APPLY_JOB, OnGetApplyJob)
+  // yield takeEvery(DELETE_APPLY_JOB, OnDeleteApplyJob)
 }
                                       
 export default careerSaga;
