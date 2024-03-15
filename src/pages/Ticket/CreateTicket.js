21ff7c01336870b1e903a90f019198d883acbe0e
lotus-npv <lotus.npv@gmail.com>
2024-03-15 10:29:20 +0700

    new commit

--- src/pages/Ticket/CreateTicket.js
+++ src/pages/Ticket/CreateTicket.js
@@ -1,31 +1,40 @@
 import React, { useEffect } from "react";
+import { Card, CardBody, Container, Button, CardHeader, Row, Col, UncontrolledTooltip } from "reactstrap";
 
 //redux
 import { useSelector, useDispatch } from "react-redux";
 
 // actions
-import { getDemoData } from "../store/actions";
+import { getDemoData } from "../../store/actions";
 
 const CreateTicket = (props) => {
-  const dispatch = useDispatch();
-  const { demoData } = useSelector(state => ({
-    demoData: state.Demo.demoData,
-  }));
+    const dispatch = useDispatch();
+    const { demoData } = useSelector(state => ({
+        demoData: state.Demo.demoData,
+    }));
 
-  /*
-  get data
-  */
-  useEffect(() => {
-    dispatch(getDemoData());
-  }, [dispatch]);
+    /*
+    get data
+    */
+    useEffect(() => {
+        dispatch(getDemoData());
+    }, [dispatch]);
 
-  // your API's response data will be in events variable.
-  console.log(demoData);
+    // your API's response data will be in events variable.
+    console.log(demoData);
 
-  return (
-    <div>
-    </div>
-  );
+    return (
+        <div className="page-content">
+            <Container fluid={true}>
+                <div>
+                    {demoData.map(item => (
+                        <p key={item.id}>{item.id} {' - '} {item.title}</p>
+                    ))}
+                </div>
+            </Container>
+        </div>
+
+    );
 };
 
 export default CreateTicket;
