351948a951b702babf3efe4a9e493c9bb73b1e87
lotus-npv <lotus.npv@gmail.com>
2024-03-18 22:46:50 +0700

    new commit

--- src/pages/Intern/TableDatas.js
+++ src/pages/Intern/TableDatas.js
@@ -15,6 +15,8 @@ import ModalDatas from './ModalDatas'
 import { withTranslation } from "react-i18next";
 import PropTypes from "prop-types";
 
+import avata from '../../assets/images/avata/avata-loading.png';
+
 // //redux
 import { useSelector, useDispatch, shallowEqual } from "react-redux";
 import { getInternAll, updateIntern, deleteIntern, setIntern, getStatusAll, getStatusDetailAll } from "store/actions";
@@ -80,6 +82,12 @@ const TableDatas = (props) => {
   function removeBodyCss() {
     document.body.classList.add("no_padding");
   }
+// add new
+  const addForm = () => {
+    setRowSelect(null);
+    setIsEdit(false);
+    tog_xlarge();
+  }
 
   // //delete modal
   const [item, setItem] = useState(null);
@@ -137,11 +145,19 @@ const TableDatas = (props) => {
     );
   };
 
-  const addForm = () => {
-    setRowSelect(null);
-    setIsEdit(false);
-    tog_xlarge();
-  }
+// import avata from '../../assets/images/avata/avata-loading.png';
+
+  const nameBodyTemplate = (rowData) => {
+    const name = `${rowData.last_name_jp} ${rowData.first_name_jp}`;
+    const avata = rowData.avata;
+
+    return (
+        <div className="flex align-items-center gap-2">
+            <img  src={'https://i.pinimg.com/236x/0d/85/e4/0d85e4a8cd465ac49c265e29af5e53e8.jpg'} width="32" />
+            <span>{name}</span>
+        </div>
+    );
+};
 
   const actionBody = (rowData) => {
     return (
@@ -162,7 +178,7 @@ const TableDatas = (props) => {
       <DataTable value={internDataAll} paginator rows={15} stripedRows rowsPerPageOptions={[5, 10, 15, 20, 50]} dragSelection selectionMode={'multiple'} selection={selectedItems} onSelectionChange={(e) => setSelectedItems(e.value)} dataKey="id" filters={filters}
         filterDisplay="row" globalFilterFields={['id', 'name', 'description']} header={header} emptyMessage="Không tìm thấy kết quả phù hợp." tableStyle={{ minWidth: '50rem' }} scrollable scrollHeight={vh} size={'small'}>
         <Column selectionMode="multiple" exportable={false} headerStyle={{ width: '3rem' }} ></Column>
-        <Column field="last_name_jp" header="Tên thực tập sinh" filterField="name" filter filterPlaceholder="Tìm kiếm bằng tên" sortable style={{ minWidth: '12rem' }} ></Column>
+        <Column field="name" header="Tên thực tập sinh" filterField="name" body={nameBodyTemplate} filter filterPlaceholder="Tìm kiếm bằng tên" sortable style={{ minWidth: '12rem' }} ></Column>
         <Column field="factory" header="Xí nghiệp" filterField="factory" filter filterPlaceholder="Tìm kiếm bằng tên" sortable style={{ minWidth: '12rem' }} ></Column>
         <Column field="company" header="Phái cử" filterField="company" filter filterPlaceholder="Tìm kiếm bằng tên" sortable style={{ minWidth: '12rem' }} ></Column>
         <Column field="residence" header="Tư cách lưu trú" filterField="residence" filter filterPlaceholder="Tìm kiếm bằng tên" sortable style={{ minWidth: '12rem' }} ></Column>
