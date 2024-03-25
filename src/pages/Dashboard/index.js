9b2b57cb60a23d2ee1ff732e4d8078626ae7a14e
lotus-npv <lotus.npv@gmail.com>
2024-03-25 13:01:49 +0700

    new commit

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,5 +1,5 @@
-import PropTypes from "prop-types";
-import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types"
+import React, { useEffect, useState } from "react"
 import {
   Container,
   Row,
@@ -13,93 +13,137 @@ import {
   ModalBody,
   ModalFooter,
   Table,
-  UncontrolledTooltip
-} from "reactstrap";
-import { Link } from "react-router-dom";
+  UncontrolledTooltip,
+} from "reactstrap"
+import { Link } from "react-router-dom"
 
-import ReactApexChart from "react-apexcharts";
-import getChartColorsArray from "../../components/Common/ChartsDynamicColor";
+import ReactApexChart from "react-apexcharts"
+import getChartColorsArray from "../../components/Common/ChartsDynamicColor"
 
-import classNames from "classnames";
+import classNames from "classnames"
 
 //import Charts
-import StackedColumnChart from "./StackedColumnChart";
+import StackedColumnChart from "./StackedColumnChart"
 
 //import action
-import { getChartsData as onGetChartsData } from "../../store/actions";
+import { getChartsData as onGetChartsData } from "../../store/actions"
 
-import modalimage1 from "../../assets/images/product/img-7.png";
-import modalimage2 from "../../assets/images/product/img-4.png";
+import modalimage1 from "../../assets/images/product/img-7.png"
+import modalimage2 from "../../assets/images/product/img-4.png"
 
 // Pages Components
-import Transactions from "./transactions";
-import Notifications from "./notifications";
-
+import Transactions from "./transactions"
+import Notifications from "./notifications"
 
 //i18n
-import { withTranslation } from "react-i18next";
-import { useNavigate } from "react-router-dom";
+import { withTranslation } from "react-i18next"
+import { useNavigate } from "react-router-dom"
 
 //redux
-import { useSelector, useDispatch } from "react-redux";
-import { createSelector } from "reselect";
+import { useSelector, useDispatch, shallowEqual } from "react-redux"
+import { getInternAllInfo } from "../../store/actions"
+import { createSelector } from "reselect"
+import { yearData } from "common/data"
 
 const Dashboard = props => {
-  const navigate = useNavigate();
-  const [modal, setmodal] = useState(false);
-  const [subscribemodal, setSubscribemodal] = useState(false);
+  const navigate = useNavigate()
+  const [modal, setmodal] = useState(false)
+  // const [subscribemodal, setSubscribemodal] = useState(false);
 
-  const selectDashboardState = (state) => state.Dashboard;
+  const selectDashboardState = state => state.Dashboard
   const DashboardProperties = createSelector(
     selectDashboardState,
-    (dashboard) => ({
-      chartsData: dashboard.chartsData
+    dashboard => ({
+      chartsData: dashboard.chartsData,
     })
-  );
+  )
 
-  const {
-    chartsData
-  } = useSelector(DashboardProperties);
+  const { chartsData } = useSelector(DashboardProperties)
+
+  // The intern's visa is about to expire
 
   const reports = [
-    { title: "The item needing support has not been processed yet", iconClass: "bx bx-support", description: "3" , value: 1},
-    { title: "The intern's visa is about to expire", iconClass: "bx bx-user", description: "12" , value: 2},
+    {
+      title: "The item needing support has not been processed yet",
+      iconClass: "bx bx-support",
+      description: "3",
+      value: 1,
+    },
+    {
+      title: "The intern's visa is about to expire",
+      iconClass: "bx bx-user",
+      description: "12",
+      value: 2,
+    },
     {
       title: "Interns are about to enter the country",
       iconClass: "bx bx-user",
-      description: "5", value: 3
+      description: "5",
+      value: 3,
     },
-  ];
+  ]
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setSubscribemodal(true);
+  //   }, 2000);
+  // }, []);
+
+  const [periodData, setPeriodData] = useState([])
+  const [periodType, setPeriodType] = useState("yearly")
 
   useEffect(() => {
-    setTimeout(() => {
-      setSubscribemodal(true);
-    }, 2000);
-  }, []);
+    setPeriodData(chartsData)
+  }, [chartsData])
 
-  const [periodData, setPeriodData] = useState([]);
-  const [periodType, setPeriodType] = useState("yearly");
-
-  useEffect(() => {
-    setPeriodData(chartsData);
-  }, [chartsData]);
-
-  const [isMonth, setIsMonth] = useState("btn-group btn-group-sm  d-flex justify-center  d-none")
+  const [isMonth, setIsMonth] = useState(
+    "btn-group btn-group-sm  d-flex justify-center  d-none"
+  )
 
   const onChangeChartPeriod = pType => {
-    setPeriodType(pType);
-    dispatch(onGetChartsData(pType));
-  };
+    setPeriodType(pType)
+    dispatch(onGetChartsData(pType))
+  }
 
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
+
   useEffect(() => {
-    dispatch(onGetChartsData("yearly"));
-  }, [dispatch]);
+    dispatch(onGetChartsData("yearly"))
+  }, [dispatch])
+
+  // get intern data
+  const { dataIntern } = useSelector(state => ({
+    dataIntern: state.Intern.datas,
+  }))
+
+  useEffect(() => {
+    dispatch(getInternAllInfo())
+  }, [dispatch])
+
+  // get thong tin thuc tap sinh theo trang thai
+  useEffect(() => {
+    if (dataIntern) {
+      console.log(dataIntern)
+    }
+  }, [dataIntern])
+
+  // get lai data sau moi 10s
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      dispatch(getInternAllInfo())
+    }, 10000)
+    // Hàm dọn dẹp khi unmount
+    return () => {
+      clearInterval(intervalId)
+    }
+  }, [])
+
+  // console.log(dataIntern)
 
   // Charst
   const dataColors = '["--bs-primary", "--bs-success", "--bs-danger"]'
-  const apexsalesAnalyticsChartColors = getChartColorsArray(dataColors);
-  const series = [56, 38, 26];
+  const apexsalesAnalyticsChartColors = getChartColorsArray(dataColors)
+  const series = [56, 38, 26]
   const options = {
     labels: ["Series A", "Series B", "Series C"],
     colors: apexsalesAnalyticsChartColors,
@@ -111,21 +155,21 @@ const Dashboard = props => {
         },
       },
     },
-  };
+  }
 
-  const handleLink = (value) => {
-    if(value == 1) {
-      return '/ticket'
-    } else if(value == 2) {
-      return '/intern'
+  const handleLink = value => {
+    if (value == 1) {
+      return "/ticket"
+    } else if (value == 2) {
+      return "/intern"
     } else {
-      return '/intern'
+      return "/intern"
     }
   }
 
   //meta title
-  document.title = "Dashboard";
-  const date = new Date();
+  document.title = "Dashboard"
+  const date = new Date()
 
   return (
     <React.Fragment>
@@ -136,7 +180,7 @@ const Dashboard = props => {
             title={props.t("Dashboards")}
             breadcrumbItem={props.t("Dashboard")}
           /> */}
-          <h4 className="fw-bold mt-1">{props.t('Need attention')}</h4>
+          <h4 className="fw-bold mt-1">{props.t("Need attention")}</h4>
           <Row>
             {/* <Col xl="4">
               <WelcomeComp />
@@ -161,11 +205,11 @@ const Dashboard = props => {
                           </div>
                           {/* flex-grow-1 */}
                           <div className="d-flex flex-column gap-2">
-                            <h3 className="mb-0 fw-bold">{report.description}</h3>
+                            <h3 className="mb-0 fw-bold">
+                              {report.description}
+                            </h3>
                             <div className="d-flex">
-                              <p className="">
-                                {props.t(report.title)}
-                              </p>
+                              <p className="">{props.t(report.title)}</p>
                             </div>
                           </div>
                         </div>
@@ -175,9 +219,15 @@ const Dashboard = props => {
                             to={handleLink(report.value)}
                             className="text-danger "
                           >
-                            <i className="bx bx-chevrons-right font-size-24 fw-bold" id="deletetooltip" />
-                            <UncontrolledTooltip placement="top" target="deletetooltip">
-                              {props.t('Detail')}
+                            <i
+                              className="bx bx-chevrons-right font-size-24 fw-bold"
+                              id="deletetooltip"
+                            />
+                            <UncontrolledTooltip
+                              placement="top"
+                              target="deletetooltip"
+                            >
+                              {props.t("Detail")}
                             </UncontrolledTooltip>
                           </Link>
                         </div>
@@ -187,15 +237,23 @@ const Dashboard = props => {
                 ))}
               </Row>
 
-              <h4 className="mt-3 fw-bold">{props.t('Detail')}</h4>
+              <h4 className="mt-3 fw-bold">{props.t("Detail")}</h4>
               <Row className="mb-3">
                 <Col xl={4}>
                   <Card className="h-100">
                     <CardBody>
                       <Row>
                         <Col xl={8}>
-                          <h4 className="card-title mb-2">{props.t('Current intern ratio')}</h4>
-                          <p>{props.t('Total:')}<span className="text-primary fw-bold">5000</span> {props.t('people')}</p>
+                          <h4 className="card-title mb-2">
+                            {props.t("Current intern ratio")}
+                          </h4>
+                          <p>
+                            {props.t("Total:")}
+                            <span className="text-primary fw-bold">
+                              5000
+                            </span>{" "}
+                            {props.t("people")}
+                          </p>
                         </Col>
                         <Col xl={4} className="d-flex justify-content-end">
                           {/* <div className="w-75">
@@ -222,7 +280,8 @@ const Dashboard = props => {
                           <Col xs="4">
                             <div className="mt-4">
                               <p className="mb-2 text-truncate">
-                                <i className="mdi mdi-circle text-primary me-1" /> Viet Nam
+                                <i className="mdi mdi-circle text-primary me-1" />{" "}
+                                Viet Nam
                               </p>
                               <h5>3,132</h5>
                             </div>
@@ -230,7 +289,8 @@ const Dashboard = props => {
                           <Col xs="4">
                             <div className="mt-4">
                               <p className="mb-2 text-truncate">
-                                <i className="mdi mdi-circle text-success me-1" /> Japan
+                                <i className="mdi mdi-circle text-success me-1" />{" "}
+                                Japan
                               </p>
                               <h5>1,763</h5>
                             </div>
@@ -238,7 +298,8 @@ const Dashboard = props => {
                           <Col xs="4">
                             <div className="mt-4">
                               <p className="mb-2 text-truncate">
-                                <i className="mdi mdi-circle text-danger me-1" /> Korean
+                                <i className="mdi mdi-circle text-danger me-1" />{" "}
+                                Korean
                               </p>
                               <h5>105</h5>
                             </div>
@@ -253,7 +314,11 @@ const Dashboard = props => {
                   <Card className="h-100">
                     <CardBody>
                       <div className="d-sm-flex flex-wrap">
-                        <h4 className="card-title mb-4">{props.t('Number of interns entering the country by country')}</h4>
+                        <h4 className="card-title mb-4">
+                          {props.t(
+                            "Number of interns entering the country by country"
+                          )}
+                        </h4>
                         <div className="ms-auto">
                           <ul className="nav nav-pills">
                             {/* <li className="nav-item">
@@ -279,12 +344,14 @@ const Dashboard = props => {
                                   "nav-link"
                                 )}
                                 onClick={() => {
-                                  onChangeChartPeriod("monthly");
-                                  setIsMonth("btn-group btn-group-sm  d-flex justify-center ")
+                                  onChangeChartPeriod("monthly")
+                                  setIsMonth(
+                                    "btn-group btn-group-sm  d-flex justify-center "
+                                  )
                                 }}
                                 id="one_month"
                               >
-                                {props.t('Month')}
+                                {props.t("Month")}
                               </Link>
                             </li>
                             <li className="nav-item">
@@ -295,45 +362,62 @@ const Dashboard = props => {
                                   "nav-link"
                                 )}
                                 onClick={() => {
-                                  onChangeChartPeriod("yearly");
-                                  setIsMonth("btn-group btn-group-sm  d-flex justify-center  d-none")
+                                  onChangeChartPeriod("yearly")
+                                  setIsMonth(
+                                    "btn-group btn-group-sm  d-flex justify-center  d-none"
+                                  )
                                 }}
                                 id="one_month"
                               >
-                                {props.t('Year')}
+                                {props.t("Year")}
                               </Link>
                             </li>
                           </ul>
-                          <div className="mt-1" style={{height: '40px'}}>
+                          <div className="mt-1" style={{ height: "40px" }}>
                             <div className={isMonth} role="group">
-                              <button type="button" className="btn btn-outline-secondary">Năm :</button>
-                              <button type="button" className="btn btn-outline-secondary">{date.getFullYear()}</button>
+                              <button
+                                type="button"
+                                className="btn btn-outline-secondary"
+                              >
+                                Năm :
+                              </button>
+                              <button
+                                type="button"
+                                className="btn btn-outline-secondary"
+                              >
+                                {date.getFullYear()}
+                              </button>
                             </div>
                           </div>
                         </div>
                       </div>
                       {/* <div className="clearfix"></div> */}
-                      <StackedColumnChart periodData={periodData} dataColors='["--bs-primary", "--bs-warning", "--bs-success"]' />
+                      <StackedColumnChart
+                        periodData={periodData}
+                        dataColors='["--bs-primary", "--bs-warning", "--bs-success"]'
+                      />
                     </CardBody>
                   </Card>
                 </Col>
               </Row>
-
-
             </Col>
           </Row>
 
           <Row>
             <Col xl="4">
-              <Transactions title={props.t('Top 5 countries by number of interns')} />
+              <Transactions
+                title={props.t("Top 5 countries by number of interns")}
+              />
             </Col>
             <Col xl="4">
-              <Transactions title={props.t('Top 5 nominated by number of interns')} />
+              <Transactions
+                title={props.t("Top 5 nominated by number of interns")}
+              />
             </Col>
 
-            <Col xl="4" >
+            <Col xl="4">
               {/* <Transactions title={'Top 5 xí nghiệp theo số lượng TTS'} /> */}
-              <Notifications title={props.t('Latest announcement')} />
+              <Notifications title={props.t("Latest announcement")} />
             </Col>
           </Row>
 
@@ -344,16 +428,14 @@ const Dashboard = props => {
           </Row> */}
         </Container>
       </div>
-
-
     </React.Fragment>
-  );
-};
+  )
+}
 
 Dashboard.propTypes = {
   t: PropTypes.any,
   chartsData: PropTypes.any,
   onGetChartsData: PropTypes.func,
-};
+}
 
-export default withTranslation()(Dashboard);
+export default withTranslation()(Dashboard)
